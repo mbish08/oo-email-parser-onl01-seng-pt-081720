e7cc54927d24446baf54a4471a6e9516021af2fa
Mikel Bishop <mdrew8@gmail.com>
2020-09-13 21:32:05 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -5,14 +5,14 @@
 
 class EmailAddressParser 
   
- attr_accessor :emails 
+ attr_accessor :email 
  
   def initialize
-    @emails = emails
+    @email = emails
   end 
   
   def parse
-    @emails.split(/[, ]/)
+    @email.split(/[, ]/)
     
     
   end 
